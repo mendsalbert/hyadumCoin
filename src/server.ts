0f3140734b51239b1e67a9b36136a8f4dd6844a1
graphica99 <mendsalbert@gmail.com>
2022-03-02 11:32:56 +0000

    api for blockchain

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from "express";
+import Blockchain from "./blockchain";
+const app: Application = express();
+
+const blockchain = new Blockchain();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/blocks", (req: Request, res: Response) => {
+  res.status(200).json({
+    blockchain: blockchain,
+  });
+});
+
+app.post("/api/mine", (req: Request, res: Response) => {
+  const { data } = req.body;
+  blockchain.addBlock({ data });
+  res.redirect("/api/blocks");
+});
+
+try {
+  app.listen(port, (): void => {
+    console.log(`Connected successfully on port ${port}`);
+  });
+} catch (error: any) {
+  console.error(`Error occured: ${error.message}`);
+}
