a6d2c6f79fa96df5afc132ffb181fd0dbb49c3a7
graphica99 <mendsalbert@gmail.com>
2022-03-04 04:46:07 +0000

    pubsub

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import express, { Application, Request, Response } from "express";
 import Blockchain from "./blockchain";
 import PubSub from "./pubsub";
+const request = require("request");
 const app: Application = express();
 
 const blockchain = new Blockchain();
+
 const DEFAULT_PORT = 3000;
 let PEER_PORT;
 if (process.env.GENERATE_PEER_PORT === "true") {
   PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 const port = PEER_PORT || DEFAULT_PORT;
-
+const ROOT_NODE_ADDERSS = `http://localhost:${DEFAULT_PORT}`;
 const pubsub = new PubSub({ blockchain });
 
 setTimeout(() => {
@@ -32,8 +34,22 @@ app.post("/api/mine", (req: Request, res: Response) => {
   res.redirect("/api/blocks");
 });
 
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDERSS}/api/blocks` },
+    (error: any, response: any, body: any) => {
+      if (!error && response.statusCode === 200) {
+        const chain = JSON.parse(body);
+        console.log(chain);
+        blockchain.replaceChain(chain);
+      }
+    }
+  );
+};
+
 try {
   app.listen(port, (): void => {
+    syncChains();
     console.log(`Connected successfully on port ${port}`);
   });
 } catch (error: any) {
