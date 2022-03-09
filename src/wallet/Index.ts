89c47b0c7e630b1b130eba40cb8731f45e2b0011
graphica99 <mendsalbert@gmail.com>
2022-03-09 04:09:13 +0000

    calculate balance function

--- src/wallet/Index.ts
+++ src/wallet/Index.ts
@@ -25,6 +25,22 @@ class Wallet {
       return new Transaction(this, recipient, amount);
     }
   }
+
+  static calculateBalance(chain: any, address: string) {
+    let totalBalance: any;
+    for (let i = 0; i < chain.length; i++) {
+      let block = chain[i];
+
+      for (let transaction of block.data) {
+        let addressOutput = transaction.outputMap[address];
+        if (addressOutput) {
+          totalBalance = totalBalance + addressOutput;
+        }
+      }
+    }
+
+    return STARTING_BALANCE + totalBalance;
+  }
 }
 
 export default Wallet;
